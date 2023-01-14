cf8c9df6fd3099a9b67e7a6cc58a3327f69bfe1a
Patrick Rios <angleopatrickriosnolasco@gmail.com>
2023-01-13 23:11:44 -0500

    project build

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5173',
-    'http://localhost:3500',
-    'http://localhost:3001'
+  "https://donvoid18.github.io/Proyecto-Front-End/",
+  "http://localhost:5173",
+  "https://www.yoursite.com",
+  "http://localhost:3500",
+  "http://127.0.0.1:5174",
 ];
 
 module.exports = allowedOrigins;
